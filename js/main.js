02ee8850f1507edd2effc0525c5de17423e5dcfd
Rajwantkaur <43739725+Rajwantkaur@users.noreply.github.com>
2023-09-09 18:56:57 -0400

    Home page update

--- js/main.js
+++ js/main.js
@@ -792,32 +792,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const projectData = [
     {
       title: "FBG Job Alerts",
-      description: "This comprehensive platform aggregates various job portals to assist job seekers in receiving tailored email alerts for positions matching their interests and preferred locations. What sets FBG Job Alerts apart is its ability to seamlessly import and process millions of job seeker and job listing data daily,and its versatile data export capabilities in various formats.\n\n<b>Tech Stack:</b> HTML, CSS, Angular, JavaScript, Java, Spring Boot, AWS Services",
+      description: "<b  id=\"projectOverview\">Overview: \n</b>This comprehensive platform aggregates various job portals to assist job seekers in receiving tailored email alerts for positions matching their interests and preferred locations. What sets FBG Job Alerts apart is its ability to seamlessly import and process millions of job seeker and job listing data daily,and its versatile data export capabilities in various formats.\n\n<b id=\"techStack\">Tech Stack:</b> HTML, CSS, Angular, JavaScript, Java, Spring Boot, AWS Services",
       thumbnail: "img/oakjobalerts_logo.png",
       srcURL: "https://cylconjobs.com/",
     },
     {
       title: "Fashion Boutique",
-      description: "Fashion Boutique is a dynamic and enticing platform for showcasing men's and women's winter fashion collections. The site boasts a responsive design, intuitive navigation, dynamic product displays, filtering options, and a fully functional shopping cart. Visitors can explore and shop for their favorite winter clothing items with ease, making it a must-visit destination for fashion enthusiasts.\n\n<b>Tech Stack:</b> HTML, CSS, JavaScript, Jquery",
+      description: "<b  id=\"projectOverview\">Overview: \n</b>Fashion Boutique is a dynamic and enticing platform for showcasing men's and women's winter fashion collections. The site boasts a responsive design, intuitive navigation, dynamic product displays, filtering options, and a fully functional shopping cart. Visitors can explore and shop for their favorite winter clothing items with ease, making it a must-visit destination for fashion enthusiasts.\n\n<b id=\"techStack\">Tech Stack:</b> HTML, CSS, JavaScript, Jquery",
       thumbnail: "img/fashion_home2.png",
       srcURL: "https://rajwant3.github.io/FashionBoutique/index.html",
     },
     {
       title: "Flappy Bird",
       description:
-        "This project is a replica of Flappy Bird game and quite similar to dinosaur game of Chrome browser, where a bird keeps jumping until it hits an obstacle. Players control a bird using the spacebar to flap its wings, and the game ends if the bird hits obstacles—pipes placed both above and below. The player's score is based on how many obstacles they successfully pass.\n\n<b>Tech Stack:</b> Python,MySQl,Tkinter,Pygame",
+        "<b  id=\"projectOverview\">Overview: \n</b>This project is a replica of Flappy Bird game and quite similar to dinosaur game of Chrome browser, where a bird keeps jumping until it hits an obstacle. Players control a bird using the spacebar to flap its wings, and the game ends if the bird hits obstacles—pipes placed both above and below. The player's score is based on how many obstacles they successfully pass.\n\n<b id=\"techStack\">Tech Stack:</b> Python,MySQl,Tkinter,Pygame",
       thumbnail: "img/flappy2.png",
       srcURL: "https://github.com/Rajwant3/FlappyBird",
     },
     {
       title: "OnMo Videos",
       description:
-        "OnMo Videos was a premium video platform developed in multiple technologies. It provided users with a rich library of content, accessible through mobile network billing. With personalized recommendations and multi-device support, it offered a seamless and convenient entertainment experience.However, At present this is not live.\n\n<b>Tech Stack:</b> Java,Spring boot, Cloud Search, AWS, RDS",
+        "<b  id=\"projectOverview\">Overview: \n</b>OnMo Videos was a premium video platform developed in multiple technologies. It provided users with a rich library of content, accessible through mobile network billing. With personalized recommendations and multi-device support, it offered a seamless and convenient entertainment experience.However, At present this is not live.\n\n<b id=\"techStack\">Tech Stack:</b> Java,Spring boot, Cloud Search, AWS, RDS",
       thumbnail: "img/Onmovideos_logo2.png",  
     }, {
       title: "Rk Electronics",
       description:
-        "The RK Electronics project, developed during college term 1, is an e-commerce platform for electronic products. Built using HTML, CSS, and JavaScript, this platform offers a user-friendly interface for browsing and purchasing electronic gadgets.\n\n<b>Tech Stack:</b> HTML,CSS, JavaScript",
+        "<b  id=\"projectOverview\">Overview: \n</b>The RK Electronics project, developed during college term 1, is an e-commerce platform for electronic products. Built using HTML, CSS, and JavaScript, this platform offers a user-friendly interface for browsing and purchasing electronic gadgets.\n\n<b  id=\"techStack\">Tech Stack:</b> HTML,CSS, JavaScript",
       thumbnail: "img/rkelectronics_home2.png",
       srcURL: "https://rajwant3.github.io/RkElectronics/",
     }, 
@@ -877,7 +877,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
     // Call the autoSlide function to start automatic image changing on load
-    autoSlide();
+    // autoSlide();
 });
 
 
@@ -927,24 +927,24 @@ async function handleSubmit(event) {
   }
 }
 
-form.addEventListener("submit", handleSubmit);
+// form.addEventListener("submit", handleSubmit);
 
 
 
 // Event listeners for the navigation buttons
-prevButton.addEventListener('click', () => {
-    if (sliderCurrentIndex > 0) {
-      sliderCurrentIndex--;
-        updateSlider();
-    }
-});
+// prevButton.addEventListener('click', () => {
+//     if (sliderCurrentIndex > 0) {
+//       sliderCurrentIndex--;
+//         updateSlider();
+//     }
+// });
 
-nextButton.addEventListener('click', () => {
-    if (sliderCurrentIndex < slider.children.length - 1) {
-      sliderCurrentIndex++;
-        updateSlider();
-    }
-});
+// nextButton.addEventListener('click', () => {
+//     if (sliderCurrentIndex < slider.children.length - 1) {
+//       sliderCurrentIndex++;
+//         updateSlider();
+//     }
+// });
 // Function to update the slider's position
 function updateSlider() {
   const offset = -currentIndex * 100;
